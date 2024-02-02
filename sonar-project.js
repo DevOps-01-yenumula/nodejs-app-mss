0c1f0d0c621e6954766c1089cad6db0fd0f0d355
SAMBAGMG <110014836+SAMBAGMG@users.noreply.github.com>
2024-02-02 10:22:33 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.219.30.153:9000/',
+  serverUrl: 'http://34.200.232.198:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
